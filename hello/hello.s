f5aa8a651eab6f8c198738888f2768838584fdc0
karanagi <karanagi@wlp1s0.net>
2025-04-16 18:55:53 -0700

    Added hello

---
+++ hello/hello.s
@@ -0,0 +1,31 @@
+.global _start
+
+.equ SYS_WRITE, 64
+.equ SYS_EXIT, 93
+
+.equ STDOUT_FILENO,	0
+
+.equ EXIT_SUCCESS,	0
+
+.section .text
+_start:
+	# write syscall
+	li	a0, STDOUT_FILENO
+	la	a1, msg
+
+	la	t0, msgend
+	la	t1, msg
+	sub	a2, t0, t1
+
+	li	a7, SYS_WRITE
+	ecall
+
+	# exit syscall
+	li	a0, EXIT_SUCCESS
+	li	a7, SYS_EXIT
+	ecall
+
+.section .rodata
+msg: 
+	.ascii "Hello RISC-V!\n"
+msgend:
